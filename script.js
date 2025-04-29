6958658309b5863d768852222961487c861044d8
Marcos <133831775+MarcosFeitosa0408@users.noreply.github.com>
2025-04-29 13:37:48 -0300

    Update script.js
    Atualização

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// Só para futuras animações de clique, exibições de projetos etc.
-// Aqui podemos fazer animações de botões, cards etc. depois se quiser.
+document.addEventListener("DOMContentLoaded", () => {
+  const barras = document.querySelectorAll(".progresso");
+  barras.forEach(barra => {
+    const largura = barra.getAttribute("style").match(/\\d+/)[0];
+    setTimeout(() => {
+      barra.style.width = largura + "%";
+    }, 300);
+  });
+});
 
 console.log("Site do Marcos carregado com sucesso!");
 
-document.addEventListener("DOMContentLoaded", () => {
-    const barras = document.querySelectorAll(".progresso");
-    barras.forEach(barra => {
-      const largura = barra.getAttribute("style").match(/\\d+/)[0];
-      barra.style.width = largura + "%";
-    });
-  });
 
