59980405d24ca10fd7b69f3fb2a1f363da4067a9
Marcos <133831775+MarcosFeitosa0408@users.noreply.github.com>
2025-04-29 11:36:27 -0300

    Primeiro envio do site

---
+++ script.js
@@ -0,0 +1,4 @@
+// Só para futuras animações de clique, exibições de projetos etc.
+// Aqui podemos fazer animações de botões, cards etc. depois se quiser.
+
+console.log("Site do Marcos carregado com sucesso!");
