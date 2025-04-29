cb82505fa1747f516b36d226201a55d72866005e
Marcos <133831775+MarcosFeitosa0408@users.noreply.github.com>
2025-04-29 12:38:11 -0300

    Update script.js
    Alteração

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 // Aqui podemos fazer animações de botões, cards etc. depois se quiser.
 
 console.log("Site do Marcos carregado com sucesso!");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const barras = document.querySelectorAll(".progresso");
+    barras.forEach(barra => {
+      const largura = barra.getAttribute("style").match(/\\d+/)[0];
+      barra.style.width = largura + "%";
+    });
+  });
+
